0f0ffec639b3bb87ec1b6871e855da98b282914c
Neel Smith <dnsmith.neel@gmail.com>
2015-05-13 06:52:33 -0400

    Add java suite

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+
+# Repository for 21st century version of gradle:  
+add-apt-repository ppa:cwchien/gradle
 apt-get update
 
+
+# version control
+apt-get install -y git
+GIT=`which git`
+echo Installed git at $GIT
+
+
+# XML editor
 apt-get install -y xmlcopyeditor
 
+
+# JDK dev bundle
+apt-get install -y openjdk-7-jdk
+apt-get -y install groovy
+apt-get -y install gradle
 
 
 # Remove unneeded apps:
