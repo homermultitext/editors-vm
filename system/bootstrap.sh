4af31d7e26f4104b26aace62e9f4af70f256100e
Neel Smith <dnsmith.neel@gmail.com>
2015-05-13 06:36:44 -0400

    Rm unused apps

--- system/bootstrap.sh
+++ system/bootstrap.sh
@@ -5,3 +5,20 @@ apt-get update
 apt-get install -y xmlcopyeditor
 
 
+
+# Remove unneeded apps:
+apt-get -y remove transmission
+apt-get -y remove sylpheed
+apt-get -y remove mtpaint
+apt-get -y remove simple-scan
+apt-get -y remove audacious
+apt-get -y remove guvcview
+apt-get -y remove gnome-mplayer
+apt-get -y remove xfburn
+apt-get -y remove abiword
+apt-get -y remove gnumeric
+apt-get -y remove pidgin
+apt-get -y remove transmission-gtk
+
+
+
