1c61a4ed1192d88e762753187a5ff8f4b7f99128
Neel Smith <dnsmith.neel@gmail.com>
2017-06-05 16:47:27 -0400

    Fix typo

--- scripts/atom-conf.sh
+++ scripts/atom-conf.sh
@@ -1,4 +1,9 @@
 # Configure atom
+apm install intentions
+apm install busy-signal
+apm install linter
+apm install linter-ui-default
 apm install linter-autocomplete-jing
+
 cd /vagrant/atomic-tei
 apm link
