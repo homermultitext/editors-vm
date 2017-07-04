3f4a9fdc6432e3946b47bb205981701d23b6b168
Neel Smith <neelsmith@users.noreply.github.com>
2017-07-04 09:59:30 -0400

    Update refresh-hmt.sh

--- scripts/refresh-hmt.sh
+++ scripts/refresh-hmt.sh
@@ -11,17 +11,6 @@ echo git installed at $GIT
 # for editorial content.
 #
 
-if [ -d "/vagrant/hmt-mom" ]; then
-    echo "Checking for updates to HMT-MOM (verification system)"
-    cd /vagrant/hmt-mom
-    $GIT pull
-else
-    echo "Installing HMT-MOM"
-    cd /vagrant
-    echo  Running  $GIT clone https://github.com/homermultitext/hmt-mom.git
-    $GIT clone https://github.com/homermultitext/hmt-mom.git
-fi
-
 
 
 if [ -d "/vagrant/hmt-authlists" ]; then
@@ -48,36 +37,3 @@ else
 fi
 
 
-if [ -d "/vagrant/lexmapping" ]; then
-    echo "Checking for updates to modern orthographic equivalents"
-    cd /vagrant/lexmapping
-    $GIT pull
-else
-    echo "Installing modern orthographic equivalents repository"
-    cd /vagrant
-    echo  Running  $GIT clone https://github.com/homermultitext/lexmapping.git
-    $GIT clone https://github.com/homermultitext/lexmapping.git
-fi
-
-
-if [ -d "/vagrant/greeklang" ]; then
-    echo "Checking for updates to Kanōnes Greek morphology system"
-    cd /vagrant/greeklang
-    $GIT pull
-else
-    echo "Installing Kanōnes Greek morphology system"
-    cd /vagrant
-    echo  Running  $GIT clone https://github.com/neelsmith/greeklang.git
-    $GIT clone https://github.com/neelsmith/greeklang.git
-fi
-
-if [ -d "/vagrant/hmt-lexicon" ]; then
-    echo "Checking for updates to HMT morphological lexicon"
-    cd /vagrant/hmt-lexicon
-    $GIT pull
-else
-    echo "Installing HMT morphological lexicon"
-    cd /vagrant
-    echo  Running  $GIT clone https://github.com/homermultitext/hmt-lexicon.git
-    $GIT clone https://github.com/homermultitext/hmt-lexicon.git
-fi
